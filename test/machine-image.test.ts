8a008b488f2ab1a70a36e28db0d75dafa5378b9b
Michael Ovdiienko <mishaov@gmail.com>
2022-01-11 16:57:23 +0200

    fix: make machine-image test green

--- test/machine-image.test.ts
+++ test/machine-image.test.ts
@@ -12,11 +12,27 @@ describe("MachineImage", () => {
         region: "us-east-1",
       },
     });
-    const template = SynthUtils.toCloudFormation(stack);
+    const template = Template.fromStack(stack);
+
+    const capture = new Capture();
 
     expect(JSON.stringify(template)).toContain("ami=ami-06992628e0a8e044c");
 
-    expect(stack).toHaveResource("AWS::AutoScaling::AutoScalingGroup");
+    template.hasResourceProperties("AWS::AutoScaling::AutoScalingGroup", capture);
+  });
+
+
+  it("Should set custom instance type", () => {
+    const app = new App();
+    const stack = new MachineImageStack(app, "MachineImageStack", {
+      gitlabToken: "your gitlab token",
+      env: {
+        account: "123456789012",
+        region: "us-east-1",
+      },
+    });
+    const template = Template.fromStack(stack);
+
     expect(template).toMatchSnapshot();
   });
 });
