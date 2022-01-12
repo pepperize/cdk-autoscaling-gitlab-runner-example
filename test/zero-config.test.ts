3a1ba002b7cd9d119107537de5fb999b01d44745
Patrick Florek <patrick.florek@gmail.com>
2022-01-12 16:08:26 +0100

    fix: zero config test

--- test/zero-config.test.ts
+++ test/zero-config.test.ts
@@ -1,19 +1,43 @@
-import "@aws-cdk/assert/jest";
-import { App } from "@aws-cdk/core";
 import { ZeroConfigStack } from "../src/zero-config";
+import { Template } from "@aws-cdk/assertions";
+import { App } from "@aws-cdk/core";
 
-test("ZeroConfigStack", () => {
-  const app = new App();
-  const stack = new ZeroConfigStack(app, "ZeroConfigStack", {
-    gitlabToken: "your gitlab token",
-    env: {
-      account: "0",
-      region: "us-east-1",
-    },
+describe("ZeroConfigStack", () => {
+  it("Should have custom cache bucket", () => {
+    // Given
+    const app = new App();
+
+    // When
+    const stack = new ZeroConfigStack(app, "ZeroConfigStack", {
+      gitlabToken: "your gitlab token",
+      env: {
+        account: "0",
+        region: "us-east-1",
+      },
+    });
+
+    // Then
+    const template = Template.fromStack(stack);
+    template.hasResourceProperties("AWS::S3::Bucket", {});
+    template.hasResourceProperties("AWS::EC2::VPC", {});
+    template.hasResourceProperties("AWS::AutoScaling::AutoScalingGroup", {});
   });
 
-  expect(stack).toHaveResource("AWS::S3::Bucket");
-  expect(stack).toHaveResource("AWS::EC2::VPC");
-  expect(stack).toHaveResource("AWS::AutoScaling::AutoScalingGroup");
-  expect(app.synth().getStackArtifact(stack.artifactId).template).toMatchSnapshot();
+  it("Should match snapshot", () => {
+    // Given
+    const app = new App();
+
+    // When
+    const stack = new ZeroConfigStack(app, "ZeroConfigStack", {
+      gitlabToken: "your gitlab token",
+      env: {
+        account: "0",
+        region: "us-east-1",
+      },
+    });
+
+    // Then
+    const template = Template.fromStack(stack);
+    expect(template).toMatchSnapshot();
+  });
 });
