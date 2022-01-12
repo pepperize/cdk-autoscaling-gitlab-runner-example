8eaa0095cac8103bfb414dbaff18f398b824f90d
Patrick Florek <patrick.florek@gmail.com>
2022-01-12 16:45:15 +0100

    fix: machine image stack test

--- test/machine-image.test.ts
+++ test/machine-image.test.ts
@@ -1,11 +1,13 @@
-import "@aws-cdk/assert/jest";
-import { SynthUtils } from "@aws-cdk/assert";
+import { Template } from "@aws-cdk/assertions";
 import { App } from "@aws-cdk/core";
 import { MachineImageStack } from "../src/machine-image";
 
 describe("MachineImage", () => {
   it("Should set custom instance type", () => {
+    // Given
     const app = new App();
+
+    // When
     const stack = new MachineImageStack(app, "MachineImageStack", {
       gitlabToken: "your gitlab token",
       env: {
@@ -13,11 +15,28 @@ describe("MachineImage", () => {
         region: "us-east-1",
       },
     });
-    const template = SynthUtils.toCloudFormation(stack);
 
+    // Then
+    const template = Template.fromStack(stack);
     expect(JSON.stringify(template)).toContain("ami=ami-06992628e0a8e044c");
+    template.hasResourceProperties("AWS::AutoScaling::AutoScalingGroup", {});
+  });
 
-    expect(stack).toHaveResource("AWS::AutoScaling::AutoScalingGroup");
+  it("Should match snapshot", () => {
+    // Given
+    const app = new App();
+
+    // When
+    const stack = new MachineImageStack(app, "MachineImageStack", {
+      gitlabToken: "your gitlab token",
+      env: {
+        account: "123456789012",
+        region: "us-east-1",
+      },
+    });
+
+    // Then
+    const template = Template.fromStack(stack);
     expect(template).toMatchSnapshot();
   });
 });
