d9211eb89e9e879cb3d32b3376889f5a9f9f3906
Ivan Ovdiienko <johnny.unerty@gmail.com>
2022-02-15 16:04:55 +0200

    feat: add more runner examples

--- src/multiple-runners.ts
+++ src/multiple-runners.ts
@@ -1,7 +1,11 @@
-import { GitlabRunnerAutoscaling } from "@pepperize/cdk-autoscaling-gitlab-runner";
+import {
+  GitlabRunnerAutoscaling,
+  GitlabRunnerAutoscalingJobRunnerProps,
+} from "@pepperize/cdk-autoscaling-gitlab-runner";
 import { Stack, StackProps } from "aws-cdk-lib";
 import { PolicyDocument, Role, ServicePrincipal } from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
+import { InstanceClass, InstanceSize, InstanceType } from "aws-cdk-lib/aws-ec2";
 
 /**
  * Each runner defines one `[[runners]]` section in the configuration file.
@@ -51,18 +55,96 @@ export class MultipleRunnersStack extends Stack {
       },
     });
 
+    /**
+     * Unprotected runner t3.medium unprivileged work hours 10-18:00, 30 min idle time, teardown after 20 jobs, 1 hot standby
+     */
+    const runner1: GitlabRunnerAutoscalingJobRunnerProps = {
+      // unprotected runner ?
+      configuration: {
+        token: "gitlab-token1",
+        name: "gitlab-runner1",
+        machine: {
+          maxBuilds: 20, // teardown after 20 jobs
+          idleCount: 0, // no instances except for the periods that are set at the autoscaling configuration
+          autoscaling: [
+            {
+              idleCount: 1, // 1 hot standby
+              idleTime: 1800, // 30 min idle time
+              periods: ["* * 10-18 * * *"], // work hours 10-18:00
+            },
+          ],
+        },
+        docker: {
+          privileged: false, // unprivileged
+        },
+      },
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MEDIUM), // t3.medium
+    };
+
+    /**
+     * Protected runner t3.small unprivileged, work hours 13-17:00, 15 min idle time, teardown after 5 jobs
+     */
+    const runner2: GitlabRunnerAutoscalingJobRunnerProps = {
+      // protected runner ?
+      configuration: {
+        token: "gitlab-token2",
+        name: "gitlab-runner2",
+        machine: {
+          maxBuilds: 5, // teardown after 5 jobs
+          idleCount: 0, // no instances except for the periods that are set at the autoscaling configuration
+          autoscaling: [
+            {
+              periods: ["* * 13-17 * * *"], // work hours 13-17:00
+              idleCount: 1, // 1 hot standby ?
+              idleTime: 900, // 15 min idle time
+            },
+          ],
+        },
+        docker: {
+          privileged: false, // unprivileged
+        },
+      },
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.SMALL), // t3.medium
+    };
+
+    /**
+     * Protected runner t3.xlarge, privileged, no hot standby, teardown after 1 job
+     */
+    const runner3: GitlabRunnerAutoscalingJobRunnerProps = {
+      // protected runner ?
+      configuration: {
+        token: "gitlab-token3",
+        name: "gitlab-runner3",
+        machine: {
+          maxBuilds: 1, // teardown after 1 job
+          idleCount: 0, // no hot standby
+          autoscaling: [
+            {
+              idleCount: 0, // no hot standby
+              periods: ["* * * * * *"], // always use this configuration
+            },
+          ],
+        },
+        // privileged by default
+      },
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.XLARGE), // t3.xlarge
+    };
+
     new GitlabRunnerAutoscaling(this, "Runner", {
       runners: [
+        runner1,
+        runner2,
+        runner3,
         {
           configuration: {
-            token: "token1",
+            token: "token4",
             name: "privileged-runner",
           },
           role: privilegedRole,
         },
         {
           configuration: {
-            token: "token2",
+            token: "token5",
             name: "restricted-runner",
             docker: {
               privileged: false, // Run unprivileged
